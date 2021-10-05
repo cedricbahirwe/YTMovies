f6a0ee02405aa3af5ab3eeb4baaaf598c10819b1
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-10-05 22:27:47 +0200

    initial commit

---
+++ YTMovides/YTMovides/YTMovidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  YTMovidesApp.swift
+//  YTMovides
+//
+//  Created by Cédric Bahirwe on 05/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct YTMovidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
